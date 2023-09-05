2f9ba1878ace61e251482ea8873b6852a9bb1f91
Kasika <imseriouss457@gmail.com>
2023-09-05 14:10:15 +0200

    declared global values and functions used

--- main.c
+++ main.c
@@ -4,8 +4,21 @@
 #include <time.h>
 #include <windows.h>
 
+int height=20,width=20,x,y,fx,fy,lastx,lasty;
+
+void PrintBoard();
+void Input();
+void Logic();
+void Setup();
+
+typedef struct SnakePart{
+    int x;
+    int y;
+    SnakePart* next;
+}SnakePart;
+
 int main() {
 
-    
+
     return 0;
 }
