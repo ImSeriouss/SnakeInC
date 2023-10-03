5168e28a1022d9599c813ed73532a49562a75070
Kasika <imseriouss457@gmail.com>
2023-10-03 21:54:44 +0200

    Added and implemented input

--- snake.c
+++ snake.c
@@ -4,7 +4,9 @@
 #include <time.h>
 #include <windows.h>
 
-int height=20,width=20,x,y,fx,fy,lastx,lasty;
+int height=20,width=20,x,y,fx,fy,lastx,lasty,gameover;
+enum Input{UP=0,DOWN,LEFT,RIGHT};
+enum Input input;
 
 void PrintBoard();
 void Input();
@@ -14,12 +16,17 @@ void Setup();
 typedef struct SnakePart{
     int x;
     int y;
-    SnakePart* next;
+    struct SnakePart* next;
 }SnakePart;
 
 int main() {
+    gameover = 0;
 
-
+    while (gameover==0)
+    {
+        Setup();
+        PrintBoard();
+    }
     return 0;
 }
 
@@ -31,6 +38,9 @@ void PrintBoard(){
         for(int j=0;j<width;j++) {
             if(i == 0 || i == width - 1 || j == 0 || j == height - 1) {
                 printf("#");
+            }
+            else if(i==x && j==y) {
+                printf("O");
             }
             else {
                 printf(" ");
@@ -50,4 +60,41 @@ void Setup() {
     ///// PLACING THE FRUIT ON A RANDOM COORDINATE /////
     fx = rand() % 20;
     fy = rand() % 20;
+
+
+}
+
+void Input() {
+    char c;
+
+    ///// GETTING INPUT FROM THE USER /////
+    if(kbhit()) {
+        c = getch();
+        switch(c) {
+            case 'w':
+                input = UP;
+            case 's':
+                input = DOWN;
+            case 'a':
+                input = LEFT;
+            case 'd':
+                input = RIGHT;
+        }
+    }
+}
+
+void Logic() {
+    ///// MOVING THE SNAKE HEAD ACCORDING TO THE INPUT /////
+    if(input==UP) {
+        y--;
+    }
+    else if(input==DOWN) {
+        y++;
+    }
+    else if(input==LEFT) {
+        x--;
+    }
+    else if(input==RIGHT) {
+        x++;
+    }
 }
